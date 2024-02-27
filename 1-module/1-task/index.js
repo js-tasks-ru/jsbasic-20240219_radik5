892da1fc09925d0a4f8f8f0545cd962bd6414d30
Radik Zainullin <radik.zainullin@gmail.com>
2024-02-27 11:20:40 +0300

    вычисление факториала

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,10 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i < n + 1; i++) {
+    result *= i;
+  }
+  return result;
 }
