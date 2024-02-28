252b9ccea1829a114eba5383d37ca22efc6c56f2
Radik Zainullin <radik.zainullin@gmail.com>
2024-02-28 21:46:02 +0300

    суммирование зарплат

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let result = 0;
+  for (const key in salaries) {
+    const value = salaries[key];
+    if (typeof value === "number" && isFinite(value)) {
+      result += value;
+    }
+  }
+  return result;
 }
