2d04be4268c913eb94d30c2c0c89943e2a411215
Radik Zainullin <radik.zainullin@gmail.com>
2024-03-04 12:07:27 +0300

    Переведите текст вида border-left-width в borderLeftWidth

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  const arr = str.split("-"); // массив строк
+  const result = arr.map((el, indx) =>
+    indx === 0 ? el : el.charAt(0).toUpperCase() + el.slice(1)
+  );
+  return result.join("");
 }
+
