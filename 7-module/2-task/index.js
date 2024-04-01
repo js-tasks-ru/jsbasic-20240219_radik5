570bd6f8cf962ee8f96b3cc20bed4b1a2744bbb4
Radik Zainullin <radik.zainullin@gmail.com>
2024-04-01 15:05:55 +0300

    промежуточный вариант

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,44 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Modal {
-  constructor() {
+  constructor() {}
+  open() {
+    document.body.classList.add("is-modal-open");
+    // создание modelBody
+    let modalBody = createElement(`
+    <!--Корневой элемент Modal-->
+    <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>  
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>  
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>  
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>  
+    </div>
+    `);
+
+    document.body.append(modalBody);
+  }
+  setBody(node) {
+    let modalBody = document.querySelector(".modal__body");
+    modalBody.replaceWith(node);
+  }
+  setTitle(strHeader = "") {
+    let modalTitle = document.querySelector(".modal__title");
+    modalTitle.textContent = strHeader;
+  }
+  close() {
+    document.body.classList.remove("is-modal-open");
+    this.modalBody = null;
   }
 }
